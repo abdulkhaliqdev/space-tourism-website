6959bffc5bd85b37252cf0a7bea8dc6b89970670
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2022-07-28 15:25:34 +0500

    Create Navbar for Mobile, Tablet and Desktop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header';
+import useWindowDimensions from './components/hooks/useWindowDimensions';
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Destination from './components/Destination';
+import Crew from './components/Crew';
+import Technology from './components/Technology';
+ 
+// import images
+import BackgroundImageMobile from './assets/home/background-home-mobile.jpg';
+import BackgroundImageTablet from './assets/home/background-home-tablet.jpg';
+import BackgroundImageDesktop from './assets/home/background-home-desktop.jpg';
 
 function App() {
+  const { height, width } = useWindowDimensions();
+
+  const BgImage = () => {
+    if(width<375){
+      return BackgroundImageMobile;
+    }else if(width<768){
+      return BackgroundImageTablet;
+    }else{
+      return BackgroundImageDesktop;
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundImage: `url(${BgImage()})`}}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/destination" element={<Destination />}></Route>
+          <Route path="/crew" element={<Crew />}></Route>
+          <Route path="/technology" element={<Technology />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
