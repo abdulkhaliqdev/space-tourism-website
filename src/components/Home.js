059cbd9f5f1a5dc35c512b0f8672aa560639f20c
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2022-07-30 22:46:37 +0500

    Create home page for Mobile, Tablet and Desktop version

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    
+    <div className='home-container'>
+      <div className='home-text-container'>
+        <p className='home-tagline'>So, you want to travel to</p>
+        <h1 className='home-title'>Space</h1>
+        <p className='home-paragraph'>  Let’s face it; if you want to go to space, you might as well genuinely go to 
+    outer space and not hover kind of on the edge of it. Well sit back, and relax 
+    because we’ll give you a truly out of this world experience!</p>
+      </div>
+      <div className='home-explore-container'>
+        <p>Explore</p>
+      </div>
+    </div>
   )
 }
 
